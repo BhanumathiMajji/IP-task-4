f6a054e4456aeb9c02e4497690c9387cb91d2986
BhanumathiMajji <118416242+BhanumathiMajji@users.noreply.github.com>
2023-07-23 21:49:40 +0530

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main{
+  public static void main(String[] args) 
+    {
+        int[] arr = {1, 2, 5, 5, 6, 6, 7, 2};
+
+        for (int i = 0; i < arr.length-1; i++)
+        {
+            for (int j = i+1; j < arr.length; j++)
+            {
+                if ((arr[i] == arr[j]) && (i != j))
+                {
+                    System.out.print(arr[j]);
+                }
+            }
+        }
+    }    
+}
